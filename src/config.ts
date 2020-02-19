357700a31a9d9ff98f6e9040e3bf59c0bea414f0
traaittPlatform <traaittPlatform@github.com>
2020-02-19 23:03:57 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -14,21 +14,21 @@ global.config = {
 	testnet: false,
 	coinUnitPlaces: 2,
 	txMinConfirms: 10,         // corresponds to CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE in Monero
-	txCoinbaseMinConfirms: 60, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in Monero
+	txCoinbaseMinConfirms: 120, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in Monero
 	addressPrefix: 158,
-	integratedAddressPrefix: 158,
-	addressPrefixTestnet: 33,
-	integratedAddressPrefixTestnet: 34,
-	subAddressPrefix: 52,
-	subAddressPrefixTestnet: 73,
-	feePerKB: new JSBigInt('400000000'),//20^10 - for testnet its not used, as fee is dynamic.
-	dustThreshold: new JSBigInt('1000000000'),//10^10 used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
-	defaultMixin: 12, // default value mixin
+	integratedAddressPrefix: 0,
+	addressPrefixTestnet: 0,
+	integratedAddressPrefixTestnet: 0,
+	subAddressPrefix: 0,
+	subAddressPrefixTestnet: 0,
+	feePerKB: new JSBigInt('100'),//20^10 - for testnet its not used, as fee is dynamic.
+	dustThreshold: new JSBigInt('0'),//10^10 used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
+	defaultMixin: 1, // default value mixin
 
 	idleTimeout: 30,
 	idleWarningDuration: 20,
 
-	coinSymbol: 'ETRX',
+	coinSymbol: '',
 	openAliasPrefix: "etrx",
 	coinName: 'traaittPlatform',
 	coinUriPrefix: 'traaittplatform:',
